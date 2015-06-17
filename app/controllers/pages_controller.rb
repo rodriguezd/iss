965d19d4a407bb6ffd3a39e49eeeef53dfd13436
David Rodriguez <davidrodriguez212@gmail.com>
2015-06-16 21:38:43 -0400

    Add location form.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   require 'open-uri'
+  require 'geocoder'
 
   def home
     twitter_feed = TwitterClient.client
@@ -15,6 +16,17 @@ class PagesController < ApplicationController
     json = open("https://api.twitter.com/1/statuses/oembed.json?id=#{latest_crew_tweet_id}").read
     data = JSON.parse(json)
     @iss_tweet_html = data["html"]
+  end
+
+  def sightings
+    results = Geocoder.search(params[:location])
+    latitude = results.first.latitude
+    longitude = results.first.longitude
+
+    json = open("http://api.open-notify.org/iss-pass.json?lat=#{latitude}&lon=#{longitude}&n=8").read
+    data = JSON.parse(json)
+
+    @sightings = data["response"]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'pages/home' => 'pages#home'
+  post 'sightings' => 'pages#sightings'
 
 
 end
